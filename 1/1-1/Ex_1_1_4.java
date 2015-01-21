4cc5d2dfe9b53183c62c759fd6a65f691ffb116a
2BAB <xx2bab@gmail.com>
2015-01-21 21:52:46 +0800

    add 1-1-4

---
+++ 1/1-1/Ex_1_1_4.java
@@ -0,0 +1,25 @@
+/**
+ * 1.1.4 下列语句各有什么问题（如果有的话）？
+ * <p>a.if(a > b) then c = 0;</p>
+ * <p>if a > b { c = 0 }</p>
+ * <p>if (a > b) c = 0;</p>
+ * <p>if (a > b) c = 0 else b = 0;</p>
+ *
+ * @author 2bab
+ */
+public class Ex_1_1_4 {
+
+    public static void main(String[] args) {
+
+        int a=3, b=2, c;
+
+        //if (a > b) then c = 0;          Java没有 then
+
+        //if a > b { c = 0}               Java的 if 语句需要添加 ()
+
+        if (a > b) c = 0;
+
+        //if (a > b) c = 0 else b = 0;    Java的 if 和 else 之间需要分号
+    }
+
+}
