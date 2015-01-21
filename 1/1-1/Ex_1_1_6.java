8d0e910eb0366ba429ff9c0fa1f2c71b15a9a1a8
2BAB <xx2bab@gmail.com>
2015-01-21 22:23:28 +0800

    add 1-1-6

---
+++ 1/1-1/Ex_1_1_6.java
@@ -0,0 +1,22 @@
+/**
+ * 1.1.6 下面这段程序会打印出什么？
+ *
+ * @author 2bab
+ */
+public class Ex_1_1_6 {
+
+    public static void main(String[] args) {
+
+        //斐波那契数列
+
+        int f = 0;
+        int g = 1;
+        for (int i = 0; i <= 15; i++) {
+            StdOut.println(f);
+            f = f + g;
+            g = f - g;
+        }
+
+    }
+
+}
