e3dc37aa34638e91044b20295bbe2aa45377f1b0
2BAB <xx2bab@gmail.com>
2015-01-21 17:26:58 +0800

    add 1-1-1, update folder tree

---
+++ 1/1-1/Ex_1_1_1.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * 1.1.1 给出以下表达式的值：
+ * <p>a.(0 + 15) / 2</p>
+ * <p>b.2.0e-6*100000000.1</p>
+ * <p>c.true && false || true && true</p>
+ *
+ * @author 2bab
+ */
+
+public class Ex_1_1_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println((0 + 15) / 2);// 7，整数相除取整（可尝试替换0为0.0）
+
+        System.out.println(2.0e-6*100000000.1);// 200.0000002
+
+        System.out.println(true && false || true && true);// true，"&&"运算符的优先级高于"||"
+
+    }
+
+}
