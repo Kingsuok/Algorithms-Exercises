ad16d5ec8f40f499cf986adfc727c12eef236cb1
2BAB <xx2bab@gmail.com>
2015-01-22 22:31:13 +0800

    add 1.1.12

---
+++ 1/1-1/Ex_1_1_12.java
@@ -0,0 +1,22 @@
+/**
+ * 1.1.12 以下代码会打印出什么结果？
+ *
+ * @author 2bab
+ */
+public class Ex_1_1_12 {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = 9 - i;
+        }
+        for (int i = 0; i < 10; i++) {
+            a[i] = a[a[i]];
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);// 0123443210
+        }
+
+    }
+}
