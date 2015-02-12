b0f54ffab522ca1b0e2c9c43e725a1caf4d80771
2BAB <xx2bab@gmail.com>
2015-02-12 23:57:58 +0800

    add Ex_1_1_29

---
+++ 1/1-1/Ex_1_1_29.java
@@ -0,0 +1,15 @@
+/**
+ * 1.1.29 等值键。为 BinarySearch 类添加一个静态方法 rank()，它接受一个键和一个整型有序数组
+ * (可能存在重复键) 作为参数并返回数组中小于该键的元素数量，以及一个类似的方法 count() 来返回
+ * 数组中等于该键的元素的数量。注意：如果i和j分辨是 rank(key, a) 和 count(key, a) 的返回值
+ * 那么a[i..i+j-1]就是数组中所有和key相等的元素。
+ *
+ * @author 2bab
+ */
+public class Ex_1_1_29 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
